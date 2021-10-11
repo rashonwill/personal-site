8de513372d15c3994c9bcfe735d68245ae12d311
rashonwill <75400387+rashonwill@users.noreply.github.com>
2021-10-11 11:21:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -149,10 +149,10 @@ const App = () => {
         <h1>Contact Me</h1>
         <div class="contact-form">
           <form class="contact-me" action="https://formsubmit.co/williams_rashon92@yahoo.com" method="POST">
-            <input type="text" placeholder="Your Name"></input>
-            <input type="text" placeholder="Email"></input>
-            <input type="text" placeholder="Subject"></input>
-            <textarea placeholder="Let's Connect"></textarea>
+            <input type="text" name="name" placeholder="Your Name"></input>
+            <input type="text" name="email" placeholder="Email"></input>
+            <input type="text" name="subject" placeholder="Subject"></input>
+            <textarea name="message" placeholder="Let's Connect"></textarea>
       <div class="image">
         <div class="linked">
     <i class="fa fa-linkedin"></i>
