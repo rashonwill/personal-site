d6f97cee856e2a8f41769d1e37cda9cdefc5c604
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-01-26 14:31:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -350,7 +350,7 @@ viewEducation();
             <Card.Body>
               <Card.Title style={{ fontSize: "25px" }}>Spotify</Card.Title>
               <Card.Text style={{ fontSize: "22px" }}>
-                Spotify API Project, check out some of your favorite jams, on our custom app. 
+                Spotify API Project, check out some of your favorite jams. 
               </Card.Text>
 
  <div class="icons">
