183bf786b33c71963b9c65ab5e5c6fe2f21b68ad
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-01-31 13:49:19 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -309,7 +309,7 @@ viewEducation();
                 Hottest eCommerce apparel store, for your apparel needs. Stay Fresh. Stay Crisp. Sneaker Head.
               </Card.Text>
               <Button variant="primary">
-                <a href="https://sneakerheads22.netlify.app/" target="_blank">Visit Sneaker Head</a>
+                <a href="https://sneakerhead22.herokuapp.com/" target="_blank">Visit Sneaker Head</a>
               </Button>
             </Card.Body>
           </Card>
