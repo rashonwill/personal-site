eceef54c5fd65d4c14ff77878d9fc6936d478e29
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-02-09 15:27:09 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -304,7 +304,7 @@ viewEducation();
               src="https://ih1.redbubble.net/image.1028635848.2482/st,small,507x507-pad,600x600,f8f8f8.jpg"
             />
             <Card.Body>
-              <Card.Title style={{ fontSize: "25px" }}>Sneaker Head (ReactJS Application)</Card.Title>
+              <Card.Title style={{ fontSize: "25px" }}>Sneaker Head (ReactJS)</Card.Title>
               <Card.Text style={{ fontSize: "22px" }}>
                 Hottest eCommerce apparel store, for your apparel needs. Stay Fresh. Stay Crisp. Sneaker Head.
               </Card.Text>
