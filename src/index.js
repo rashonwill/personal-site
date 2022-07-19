4ab6dafe76381b0a40340552a86992dffa1cca89
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-07-19 11:22:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -331,11 +331,11 @@ viewEducation();
               </Card.Text>
               <div class="icons">
                 <div class="ios">
-                <i class="fa fa-apple" aria-hidden="true"></i>
+                <i class="fa fa-apple" aria-hidden="true"><a href="https://snack.expo.dev/@shonwill/fariapp" target="_blank"></a></i>
 <a href="https://snack.expo.dev/@shonwill/fariapp" target="_blank">iOS App</a>
 </div>
 <div class="android">
-                <i class="fa fa-android" aria-hidden="true"></i>
+                <a href="https://snack.expo.dev/@shonwill/fariapp" target="_blank"><i class="fa fa-android" aria-hidden="true"></i></a>
 <a href="https://snack.expo.dev/@shonwill/fariapp" target="_blank">Android App</a>
 </div>
                 </div>
