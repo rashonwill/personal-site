18641ed817a922c2bea3f5cd40ec5fae182dbcc0
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-08-16 15:39:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -173,7 +173,30 @@ viewEducation();
 
         </div>
  <Modal
-        style={{ opacity: 1, backgroundColor: null, background: null}}
+        style={{
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: none
+    },
+    content: {
+      position: 'absolute',
+      top: '40px',
+      left: '40px',
+      right: '40px',
+      bottom: '40px',
+      border: none,
+      background: '#fff',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '4px',
+      outline: 'none',
+      padding: '20px'
+    }
+}}
         isOpen={resume}
         onRequestClose={() => setResume(false)}
       >
